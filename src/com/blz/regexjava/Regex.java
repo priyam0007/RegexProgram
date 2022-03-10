484185ba6d746148e7b8112319ba72e17eba41f3
Priya <Priyamadiwalar0007@gmail.com>
2022-03-11 00:21:33 +0530

    Added:Enter a Valid First Name

--- src/com/blz/regexjava/Regex.java
+++ src/com/blz/regexjava/Regex.java
@@ -1,8 +1,31 @@
 package com.blz.regexjava;
 
-public class Regex {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Regex");
+class UserRegistration {
+
+	Scanner sc;
+
+	// User's First Name
+	public void firstName() {
+		sc = new Scanner(System.in);
+		System.out.print("Enter First Name :: ");
+		String FirstName = sc.next();
+		boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", FirstName);
+		if (answer) {
+			System.out.println("thank you");
+		} else {
+			System.out.println("please enter valid first name ");
+		}
+
+	}
+
+	// Driver Program
+	public class Regex {
+		public static void main(String[] args) {
+			UserRegistration userRegistration = new UserRegistration();
+			userRegistration.firstName();
+		}
 	}
 }
